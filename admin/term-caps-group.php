67968186f636123da4dc66d2db3877825ab6fa44
pglewis <pglewis13@gmail.com>
2013-10-31 06:54:01 -0400

    Added group and taxonomy skeleton classes

---
+++ admin/term-caps-group.php
@@ -0,0 +1,12 @@
+<?php
+
+class TermCapsGroup {
+
+	/**
+	 * @var TermCapsTaxonomy[]
+	 */
+	public $taxonomies = array();
+
+	// ToDo: We need detials on how we'll store roles and/or caps
+
+}
---
+++ admin/term-caps-taxonomy.php
@@ -0,0 +1,37 @@
+<?php
+
+class TermCapsTaxonomy {
+
+	/**
+	 * @var stdClass $taxonomy
+	 */
+	public $taxonomy;
+
+	/**
+	 * @var bool $allow_all_terms
+	 */
+	public $allow_all_terms;
+
+	/**
+	 * @var bool $auto_enable_new_terms
+	 */
+	public $auto_enable_new_terms;
+
+	/**
+	 * @var int[] $term_ids
+	 */
+	public $term_ids;
+
+	/**
+	 * @param string $tax_name
+	 * @param int[] $term_ids
+	 * @param bool $allow_all_terms
+	 * @param bool $auto_enable_new_terms
+	 */
+	function __construct ( $tax_name, $term_ids = array(), $allow_all_terms = false, $auto_enable_new_terms = false  ) {
+		$this->taxonomy = get_taxonomy( $tax_name );
+		$this->allow_all_terms = $allow_all_terms;
+		$this->auto_enable_new_terms = $auto_enable_new_terms;
+		$this->terms = $term_ids;
+	}
+}
